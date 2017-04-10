555d322ba049f3babf31cd77c72314b84ebb70f5
CristhGunners <gunners_one@hotmail.com>
2017-04-10 14:41:24 -0300

    use number count votes

--- app/models/story.js
+++ app/models/story.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   url: DS.attr(),
-  votes: DS.hasMany('vote'),
+  votes: DS.attr('number'),
   user: DS.belongsTo('user'),
   createdAt: DS.attr('date', {
     defaultValue() { return new Date(); }
--- app/models/vote.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  user: DS.belongsTo('user'),
-  state: DS.attr('boolean', { defaultValue: false })
-});
