2208904fa0ebb2f0536d8db8e563a7bc73c0fbee
CristhGunners <gunners_one@hotmail.com>
2017-04-11 14:16:29 -0300

    rename params story detail

--- app/routes/stories/story.js
+++ app/routes/stories/story.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    return this.store.findRecord('story', params.monster_id)
+    return this.store.findRecord('story', params.story_id)
   }
 })
