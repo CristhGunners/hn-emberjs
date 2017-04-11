8e54e1bfb69e0896016557fda4f6a8a816433062
CristhGunners <gunners_one@hotmail.com>
2017-04-11 11:37:57 -0300

    add count each of stories

---
+++ tests/unit/helpers/eachcounter-test.js
@@ -0,0 +1,12 @@
+
+import { eachcounter } from 'hn-emberjs/helpers/eachcounter';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | eachcounter');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = eachcounter([42]);
+  assert.ok(result);
+});
+
