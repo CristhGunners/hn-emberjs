c616e5a09fbde72170b5180b91eb9e7bb2501fd1
CristhGunners <gunners_one@hotmail.com>
2017-04-13 18:55:03 -0300

    add voting

--- app/components/story-listing.js
+++ app/components/story-listing.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init () {
+    this._super();
+    this.set('isVoteUp', false);
+  },
+  store: Ember.inject.service(),
+  actions: {
+    voteAdd(storyItem) {
+      const story = this.get('store').peekRecord('story', storyItem.id);
+      if (this.get('isVoteUp') === true) {
+        story.decrementProperty('votes');
+        this.toggleProperty('isVoteUp');
+      } else {
+        story.incrementProperty('votes');
+        this.toggleProperty('isVoteUp');
+      }
+    }
+  }
 });
