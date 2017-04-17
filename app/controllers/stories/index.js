684c1c2566463a60eccdc26cef37bd6507f8fd6a
CristhGunners <gunners_one@hotmail.com>
2017-04-17 14:56:25 -0300

    add sorted option stories

--- app/controllers/stories/index.js
+++ app/controllers/stories/index.js
@@ -6,7 +6,15 @@ const {
 } = Ember;
 
 export default Controller.extend({
-  sortedModel: computed('model.@each.votes', function() {
-    return this.get('model').sortBy('votes').reverse();
-  })
+  sortOptions: [
+    { id: 'votes:desc', name: 'More Votes' },
+    { id: 'sortedComments:desc', name: 'More Comments' },
+    { id: 'createdAt:desc', name: 'News' }
+  ],
+  selectedSort: 'votes:desc',
+  sortProperties: Ember.computed('selectedSort', function() {
+    let selected = this.get('selectedSort');
+    return (selected ? selected.split(',') : ['votes:desc']);
+  }),
+  sortedStories: computed.sort('model', 'sortProperties')
 });
