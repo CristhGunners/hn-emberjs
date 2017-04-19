76e552ead8317c03b6a2df824320fb13a06f2834
Giovanni Collazo <hello@gcollazo.com>
2017-04-19 15:11:30 -0400

    Model loading should happen on the route, remove files that have no changes.

--- app/routes/jobs/index.js
+++ app/routes/jobs/index.js
@@ -1,11 +1,20 @@
 import Ember from 'ember';
 const {
   Route,
+  RSVP,
   $
 } = Ember;
 
+function getJob(id) {
+  return $.getJSON(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+}
+
 export default Route.extend({
   model() {
-    return $.getJSON('https://hacker-news.firebaseio.com/v0/jobstories.json');
+    return $.getJSON('https://hacker-news.firebaseio.com/v0/jobstories.json')
+      .then((jobIds) => {
+        const requests = jobIds.map(getJob);
+        return RSVP.Promise.all(requests);
+      });
   }
 });
