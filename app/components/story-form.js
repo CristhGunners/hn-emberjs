052ce5b285c1c1983948f4db59416269391e18fd
Giovanni Collazo <hello@gcollazo.com>
2017-04-19 15:37:31 -0400

    Moved story creation to component, doneSaving callback after save

--- app/components/story-form.js
+++ app/components/story-form.js
@@ -1,10 +1,29 @@
 import Ember from 'ember';
-const { Component } = Ember;
+const {
+  Component,
+  inject: { service }
+} = Ember;
 
 export default Component.extend({
+  store: service(),
+
+  formTitle: null,
+  formURL: null,
+  formUsername: null,
+
   actions: {
     save() {
-      this.attrs.save();
+      const title = this.get('formTitle');
+      const url = this.get('formURL');
+      const user = this.get('formUsername');
+
+      const model = this.get('store')
+        .createRecord('story', { title, url, user });
+
+      model.save()
+        .then(() => {
+          this.attrs.doneSaving();
+        });
     }
   }
 });
