66064f764d8656dbec75e77ca64a12146447cf6e
Giovanni Collazo <hello@gcollazo.com>
2017-04-21 14:33:43 -0400

    Add need endpoints in mirage

--- mirage/config.js
+++ mirage/config.js
@@ -1,6 +1,7 @@
 export default function() {
   this.namespace = '/api/v1';
-
   this.get('/stories');
   this.get('/stories/:id');
+  this.get('/comments/:id');
+  this.passthrough('https://hacker-news.firebaseio.com/v0/**');
 }
