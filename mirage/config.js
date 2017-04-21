1d81da08af658559e6383ca73f2635cb601742ca
CristhGunners <gunners_one@hotmail.com>
2017-04-21 16:07:32 -0300

    add mirage story create

--- mirage/config.js
+++ mirage/config.js
@@ -2,6 +2,8 @@ export default function() {
   this.namespace = '/api/v1';
   this.get('/stories');
   this.get('/stories/:id');
+  this.get('/stories/create');
+  this.post('/stories');
   this.get('/comments/:id');
   this.passthrough('https://hacker-news.firebaseio.com/v0/**');
 }
