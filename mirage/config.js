53ddd115f98f7192b2ce69f81fde1ed908fa33d7
CristhGunners <gunners_one@hotmail.com>
2017-04-23 22:19:09 -0300

    mirage functions

--- mirage/config.js
+++ mirage/config.js
@@ -2,8 +2,8 @@ export default function() {
   this.namespace = '/api/v1';
   this.get('/stories');
   this.get('/stories/:id');
-  this.get('/stories/create');
   this.post('/stories');
-  this.get('/comments/:id');
+  this.patch('/stories/:id');
+  this.post('/comments');
   this.passthrough('https://hacker-news.firebaseio.com/v0/**');
 }
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,5 +1,5 @@
 export default function(server) {
-  server.createList('story', 20);
-  server.loadFixtures('comments');
-  server.create('comment', { storyId: 1 });
+  // server.createList('story', 20);
+  // server.loadFixtures('comments');
+  // server.create('comment', { storyId: 1 });
 }
