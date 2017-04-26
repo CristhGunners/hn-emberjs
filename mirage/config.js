cd604c1166792fea48f08265bce369352fa8ae19
Giovanni Collazo <hello@gcollazo.com>
2017-04-26 17:16:47 -0400

    Enable mirage

--- mirage/config.js
+++ mirage/config.js
@@ -1,10 +1,14 @@
 export default function() {
   this.namespace = '/api/v1';
+
+  this.post('/stories');
   this.get('/stories');
   this.get('/stories/:id');
-  this.post('/stories');
   this.patch('/stories/:id');
+
   this.post('/comments');
+  this.get('/comments/:id');
+
   this.post('/users');
   this.passthrough('https://hacker-news.firebaseio.com/v0/**');
 }
