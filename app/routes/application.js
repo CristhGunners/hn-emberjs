073fd42105246f29dd53dc557e90ff3756df8502
Giovanni Collazo <hello@gcollazo.com>
2017-04-26 21:30:03 -0400

    Add route mixins

---
+++ app/routes/application.js
@@ -0,0 +1,6 @@
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import Ember from 'ember';
+
+const { Route } = Ember;
+
+export default Route.extend(ApplicationRouteMixin, {});
--- app/routes/stories/create.js
+++ app/routes/stories/create.js
@@ -1,7 +1,9 @@
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
 import Ember from 'ember';
 const { Route } = Ember;
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
   actions: {
     doneSaving() {
       this.transitionTo('stories');
